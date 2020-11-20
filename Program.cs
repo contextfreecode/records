e394fe5dc2a7ee67d70e4bad050ccfb24eeb79d6
Tom <tjpalmer@pop-os.localdomain>
2020-11-20 06:26:04 -0700

    Start toward employee demo

--- Program.cs
+++ Program.cs
@@ -5,90 +5,116 @@ using System.Text.Json;
 using static System.Console;
 
 #pragma warning disable CS0162, CS8321
-void LoadTest() {
-    var count = 100000000;
-    if (true) {
-        var structs = new RequestStruct[count];
-        for (int i = 0; i < structs.Length; i += 1) {
-            structs[i].Url = "https://rescue.org/";
-        }
-    } else {
-        var records = new Request[count];
-        for (int i = 0; i < records.Length; i += 1) {
-            records[i] = new Request { TimeoutSeconds = 30.0 };
-        }
-    }
-    Console.ReadLine();
-}
+// void LoadTest() {
+//     var count = 100000000;
+//     if (true) {
+//         var structs = new RequestStruct[count];
+//         for (int i = 0; i < structs.Length; i += 1) {
+//             structs[i].Url = "https://rescue.org/";
+//         }
+//     } else {
+//         var records = new Request[count];
+//         for (int i = 0; i < records.Length; i += 1) {
+//             records[i] = new Request { TimeoutSeconds = 30.0 };
+//         }
+//     }
+//     Console.ReadLine();
+// }
 #pragma warning restore CS0162, CS8321
 
-var headers = new Dictionary<string, string>{
-    {"Accept", "text/html,application/whatever"},
-    {"User-Agent", "ContextFreeDemo/0.0.1 (like Gecko ;)"},
-// }.ToImmutableDictionary();
-};
-var request = new DetailRequest {
-    Url = "https://rescue.org/",
-    Headers = headers,
-};
-request.Validate();
-WriteLine(request.GetHashCode());
-WriteLine(JsonSerializer.Serialize(request));
-// Change headers.
-headers["User-Agent"] = "Mozilla/5.0";
-WriteLine(request.GetHashCode());
-WriteLine(JsonSerializer.Serialize(request));
-// Create new record.
-var timeoutRequest = request with { TimeoutSeconds = 30.0 };
-WriteLine(timeoutRequest.GetHashCode());
-WriteLine(JsonSerializer.Serialize(timeoutRequest));
-WriteLine(request == timeoutRequest);
-// Change existing record.
-request.TimeoutSeconds = 30.0;
-WriteLine(request.GetHashCode());
-WriteLine(JsonSerializer.Serialize(request));
-WriteLine(request == timeoutRequest);
-WriteLine(object.ReferenceEquals(request, timeoutRequest));
+// var headers = new Dictionary<string, string>{
+//     {"Accept", "text/html,application/whatever"},
+//     {"User-Agent", "ContextFreeDemo/0.0.1 (like Gecko ;)"},
+// // }.ToImmutableDictionary();
+// };
+// var request = new DetailRequest {
+//     Url = "https://rescue.org/",
+//     Headers = headers,
+// };
+// request.Validate();
+// WriteLine(request.GetHashCode());
+// WriteLine(JsonSerializer.Serialize(request));
+// // Change headers.
+// headers["User-Agent"] = "Mozilla/5.0";
+// WriteLine(request.GetHashCode());
+// WriteLine(JsonSerializer.Serialize(request));
+// // Create new record.
+// var timeoutRequest = request with { TimeoutSeconds = 30.0 };
+// WriteLine(timeoutRequest.GetHashCode());
+// WriteLine(JsonSerializer.Serialize(timeoutRequest));
+// WriteLine(request == timeoutRequest);
+// // Change existing record.
+// request.TimeoutSeconds = 30.0;
+// WriteLine(request.GetHashCode());
+// WriteLine(JsonSerializer.Serialize(request));
+// WriteLine(request == timeoutRequest);
+// WriteLine(object.ReferenceEquals(request, timeoutRequest));
 
-WriteLine(typeof(Request).BaseType);
-WriteLine(new RequestStruct { Url = "https://rescue.org/" });
+// WriteLine(typeof(Request).BaseType);
+// WriteLine(new RequestStruct { Url = "https://rescue.org/" });
 
-record Request(
-    string? Url = null,
-    double? TimeoutSeconds = null
-) {
-    public string Url { get; init; } = Url!;
-    public double? TimeoutSeconds { get; set; } = TimeoutSeconds;
+var alice = new Employee { Name = "Alice", HireDate = new (2000, 1, 1) };
+// var alice = new Employee { Name = "Alice", HireYear = 2000 };
+alice.Validate();
+// new Employee().Validate();
+// var alice = new Employee("alice", new (2000, 1, 1));
+WriteLine(alice);
+WriteLine(alice.YearsEmployed());
+
+// string a = null!;
+// DateTime? b = (DateTime)(null as DateTime?)!;
+Box<DateTime> b = DateTime.Now;
+Box<DateTime> c = new (new (2000, 1, 1));
+
+record Box<ValueType>(ValueType Value) where ValueType : struct {
+    public static implicit operator Box<ValueType>(ValueType Value)
+        => new Box<ValueType>(Value);
+}
+
+// record Employee(string Name, DateTime HireDate);
+record Employee
+// (
+//     string? Name = null,
+//     DateTime? HireDate = null
+// )
+{
+    public string Name { get; init; } = null!;
+    public int HireYear { get; init; }
+    public DateTime HireDate { get; init; }
 
     public void Validate() {
-        if (!Url.Contains(':')) throw new ArgumentException("No scheme");
-        // WriteLine(request);
+        if (Name is null) throw new ArgumentException("Name missing");
+        // if (HireYear == 0) throw new ArgumentException("HireYear missing");
+        if (HireDate.Ticks == 0) {
+            throw new ArgumentException("HireDate missing");
+        }
     }
 
-    public string Scheme() => Url.Substring(0, Url.IndexOf(':'));
+    // public int YearsEmployed() => DateTime.Now.Year - HireYear;
+    public int YearsEmployed() => DateTime.Now.Year - HireDate.Year;
 }
 
-record DetailRequest(
-    string? Url = null,
-    double? TimeoutSeconds = null,
-    IDictionary<string, string>? Headers = null
-) : Request(Url, TimeoutSeconds) {
-    public IDictionary<string, string>? Headers { get; init; } = Headers!;
-}
+// record DetailRequest(
+//     string? Url = null,
+//     double? TimeoutSeconds = null,
+//     IDictionary<string, string>? Headers = null
+// ) : Request(Url, TimeoutSeconds) {
+//     public IDictionary<string, string>? Headers { get; init; } = Headers!;
+// }
 
-struct RequestStruct {
-    public string Url { get; set; }
-    public double? TimeoutSeconds { get; init; }
+// struct RequestStruct {
+//     public string Url { get; set; }
+//     public double? TimeoutSeconds { get; init; }
 
-    public void Validate() {
-        if (!Url.Contains(':')) throw new ArgumentException("No scheme");
-        // WriteLine(request);
-    }
+//     public void Validate() {
+//         if (!Url.Contains(':')) throw new ArgumentException("No scheme");
+//         // WriteLine(request);
+//     }
 
-    public string Scheme() => Url.Substring(0, Url.IndexOf(':'));
-}
+//     public string Scheme() => Url.Substring(0, Url.IndexOf(':'));
+// }
 
-record Square {
-    public char Row { get; init; }
-    public int Col { get; init; }
-}
+// record Square {
+//     public char Row { get; init; }
+//     public int Col { get; init; }
+// }
