299f85ecd360d890db9f5281388e074cdef23bdc
Tom <tjpalmer@pop-os.localdomain>
2020-11-21 07:14:29 -0700

    Work rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use chrono::prelude::*;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::*;
 
 fn main() {
     let alice = Employee {
@@ -7,14 +9,26 @@ fn main() {
     };
     dbg!(&alice);
     dbg!(alice.years_employed());
-    // let other_alice = Employee {
-    //     hire_year: 2010,
-    //     ..alice
-    // };
-    // dbg!(&other_alice);
+    let alice2 = Employee {
+        hire_date: Utc.ymd(2010, 1, 1),
+        ..alice
+    };
+    dbg!(&alice2);
+    let alice3 = Employee {
+        hire_date: alice.hire_date,
+        ..alice2
+    };
+    dbg!(hash(&alice));
+    dbg!(hash(&alice2));
+    dbg!(hash(&alice3));
+    dbg!(alice == alice2);
+    dbg!(alice == alice3);
 }
 
-#[derive(Debug)]
+// #[derive(Debug, Eq, Hash, PartialEq)]
+// struct EmployeeTuple<'a>(&'a str, Date<Utc>);
+
+#[derive(Debug, Eq, Hash, PartialEq)]
 struct Employee<'a> {
     name: &'a str,
     hire_date: Date<Utc>,
@@ -24,4 +38,10 @@ impl<'a> Employee<'a> {
     fn years_employed(&self) -> i32 {
         Utc::today().year() - self.hire_date.year()
     }
+}
+
+fn hash<Hashable: Hash>(hashable: &Hashable) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    hashable.hash(&mut hasher);
+    hasher.finish()
 }
